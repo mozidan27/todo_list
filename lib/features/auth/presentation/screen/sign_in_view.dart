0135b7832aa2f4041493e2be78ee3548d63e1f5a
iamzidan <zidann256@gmail.com>
2025-06-04 19:58:38 +0300

    sign in screen completed

--- lib/features/auth/presentation/screen/sign_in_view.dart
+++ lib/features/auth/presentation/screen/sign_in_view.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:todo_list/core/helper/spacing.dart';
+import 'package:todo_list/core/utils/app_assets.dart';
+import 'package:todo_list/core/utils/custom_text_style.dart';
+import 'package:todo_list/features/auth/presentation/widgets/custom_sign_in_form_field.dart';
 
 class SignInView extends StatelessWidget {
   const SignInView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Signin View')));
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SvgPicture.asset(Assets.imagesCircleImage),
+            verticalSpace(50),
+            Center(
+              child: Text(
+                "Welcome Back!",
+                style: CustomTextStyle.poppins20W500Black,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Center(child: Image.asset(Assets.imagesLoginImagePNG)),
+            CustomSignInFormField(),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/features/auth/presentation/widgets/custom_sign_in_form_field.dart
+++ lib/features/auth/presentation/widgets/custom_sign_in_form_field.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:todo_list/core/funcations/navigation.dart';
 import 'package:todo_list/core/helper/spacing.dart';
+import 'package:todo_list/core/utils/app_colors.dart';
+import 'package:todo_list/core/utils/custom_text_style.dart';
 import 'package:todo_list/core/widgets/custom_bottom.dart';
 import 'package:todo_list/core/widgets/my_custom_text_form_field.dart';
 import 'package:todo_list/features/auth/presentation/widgets/login_or_signup_text.dart';
 
-class CustomSignUpFormField extends StatelessWidget {
-  const CustomSignUpFormField({super.key});
+class CustomSignInFormField extends StatelessWidget {
+  const CustomSignInFormField({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,41 +17,47 @@ class CustomSignUpFormField extends StatelessWidget {
       child: Form(
         child: Column(
           children: [
-            MyCustomTextFormField(
-              hint: "Enter your full name",
-              label: "Enter your full name",
-              obscureText: false,
-            ),
-            verticalSpace(26),
+            verticalSpace(30),
             MyCustomTextFormField(
               hint: "Enter your email",
               label: "Enter your email",
               obscureText: false,
             ),
-            verticalSpace(26),
+            verticalSpace(30),
             MyCustomTextFormField(
               hint: "Enter your password",
               label: "Enter your password",
               obscureText: true,
             ),
-            verticalSpace(26),
-            MyCustomTextFormField(
-              hint: "Confirm your Password",
-              label: "Confirm your Password",
-              obscureText: true,
+            verticalSpace(10),
+            Padding(
+              padding: const EdgeInsets.only(left: 2.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      "ForgotPassword?",
+                      style: CustomTextStyle.poppins13W500Black.copyWith(
+                        color: AppColors.mintGreen,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            verticalSpace(33),
+            verticalSpace(30),
 
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 85.0),
-              child: MyCustomButtom(text: "Sign Up", onTap: () {}),
+              child: MyCustomButtom(text: "Sign In", onTap: () {}),
             ),
             verticalSpace(33),
-
             LogInOrSignUpText(
-              text1: "Already have an account ?",
-              text2: " Sign in",
-              onTap: () => customReplacementNavigate(context, '/login'),
+              text1: "Dont have an account ?",
+              text2: " Sign Up",
+              onTap: () => customReplacementNavigate(context, '/signup'),
             ),
           ],
         ),
